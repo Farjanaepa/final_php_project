1e812273b7eaefafdaf56368fabe6a960b87c136
Farjanaepa <farjanaepa@gmail.com>
2024-05-21 14:31:56 +0600

    update

--- POS_project/pos_project.sql
+++ POS_project/pos_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2024 at 03:52 PM
+-- Generation Time: May 21, 2024 at 10:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,28 +24,94 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(100) NOT NULL,
+  `catname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `catname`) VALUES
+(1, 'Men'),
+(2, 'Women'),
+(3, 'Kids'),
+(4, 'cosmetic'),
+(8, 'Groceries');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
   `id` int(100) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `price` float(10,2) DEFAULT NULL,
-  `manufacturer_id` varchar(50) NOT NULL
+  `name` varchar(100) NOT NULL,
+  `previous_price` int(100) NOT NULL,
+  `discount` int(100) NOT NULL,
+  `present_price` int(100) NOT NULL,
+  `cname` varchar(100) NOT NULL,
+  `sname` varchar(100) NOT NULL,
+  `quantity` varchar(100) NOT NULL,
+  `unit` varchar(100) NOT NULL,
+  `qupon` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `product_source` varchar(100) NOT NULL,
+  `quantity` varchar(100) NOT NULL,
+  `subcategory` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table `products`
 --
 
-INSERT INTO `product` (`id`, `name`, `price`, `manufacturer_id`) VALUES
-(4, 'Raju', 0.00, '5464575fghfg'),
-(5, 'Soap', 0.00, '3435465'),
-(6, 'Sari', 0.00, '3346ertrey'),
-(7, 'Shirt', 0.00, '243gvdfg'),
-(8, 'Shirt', 0.00, '3243dsfdfg'),
-(9, 'Raju', 50000.00, '3435465'),
-(11, 'Shirt', 50000.00, '3346ertre67');
+INSERT INTO `products` (`id`, `name`, `category`, `product_source`, `quantity`, `subcategory`) VALUES
+(1, 'shirt', '1', 'buy', '5', ''),
+(9, 'fdfter', '2', 'buy', '5', ''),
+(10, 'Farjana', '3', 'buy', '5', ''),
+(12, '', '', 'factory', '', ''),
+(13, '', '', 'factory', '', ''),
+(14, '', '', 'factory', '', ''),
+(15, '', '', 'factory', '', ''),
+(16, '', '', 'factory', '', ''),
+(17, '', '', 'factory', '', ''),
+(20, '', '', 'factory', '', ''),
+(21, 'Arfan', '2', 'buy', '57', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_category`
+--
+
+CREATE TABLE `sub_category` (
+  `id` int(100) NOT NULL,
+  `subcatname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sub_category`
+--
+
+INSERT INTO `sub_category` (`id`, `subcatname`) VALUES
+(3, 'shirt'),
+(4, 'sharee');
 
 -- --------------------------------------------------------
 
@@ -54,7 +120,7 @@ INSERT INTO `product` (`id`, `name`, `price`, `manufacturer_id`) VALUES
 --
 
 CREATE TABLE `suppliers` (
-  `id` int(11) NOT NULL,
+  `id` int(10) NOT NULL,
   `name` varchar(120) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
@@ -66,9 +132,9 @@ CREATE TABLE `suppliers` (
 --
 
 INSERT INTO `suppliers` (`id`, `name`, `email`, `phone`, `address`) VALUES
-(1, 'Raju', 'abalmamun2515@gmail.com', 'edtertye', ''),
-(2, 'sdfgsd', 'farjana@gmail.com', '34534', 'dhaka'),
-(3, 'Raju', 'abalmamun2515@gmail.com', '34534', 'futfy');
+(1, 'Farjana', 'farjanaepa@gmail.com', '2345678', 'Maymansing'),
+(2, 'Rafia', 'rafia@gmail.com', '345346', 'Faridpur'),
+(3, 'Raju', 'abalmamun2515@gmail.com', '34534', 'DHAKA');
 
 -- --------------------------------------------------------
 
@@ -94,9 +160,27 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sub_category`
+--
+ALTER TABLE `sub_category`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -116,16 +200,34 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
